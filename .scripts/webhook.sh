147a0aea69aaa107e779841342302fd4af57e684
Brian Mulloy <brian@landlessness.net>
2019-01-31 09:51:40 -0500

    call multiple repos to update

--- .scripts/webhook.sh
+++ .scripts/webhook.sh
@@ -3,10 +3,16 @@ body='{
 "branch":"master"
 }}'
 
-curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token ${TRAVIS_TOKEN}" \
-   -d "$body" \
-   https://api.travis-ci.com/repo/hiteapi%2Fnodejs/requests
+repositories=(nodejs react)
+
+for i in "${repositories[@]}"
+do
+    url="https://api.travis-ci.com/repo/hiteapi%2F$i/requests"
+    curl -s -X POST \
+	 -H "Content-Type: application/json" \
+	 -H "Accept: application/json" \
+	 -H "Travis-API-Version: 3" \
+	 -H "Authorization: token ${TRAVIS_TOKEN}" \
+	 -d "$body" \
+	 $url
+done
