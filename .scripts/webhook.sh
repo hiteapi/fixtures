d2a1e3a5a9065e2e429ebaccef18ca6ac97a085f
Brian Mulloy <brian@landlessness.net>
2019-01-31 08:14:54 -0500

    tweaking the api call to start tests based on fixtures changing

--- .scripts/webhook.sh
+++ .scripts/webhook.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+body='{
+"request": {
+"branch":"master"
+}}'
 
-curl -s -X POST -H "Content-Type: application/json" \
--H "Accept: application/json" -H "Travis-API-Version: 3" \
--H "Authorization: token ${TRAVIS_TOKEN}" \
--d '{"request":{"branch":"master"}}' \
-https://api.travis-ci.org/repo/hiteapi%2Fnodejs/requests
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token ${TRAVIS_TOKEN}" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/hiteapi%2Fnodejs/requests
